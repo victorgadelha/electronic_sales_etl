c85c7f23a29fce7b7054f4baad7d0f1c628f064a
Victor Gadelha <joaovictor_gadelha@live.com>
2026-04-22 14:20:00 -0300

    feat: transformando raw CSV em Parquet

---
+++ src/extract.py
@@ -0,0 +1,15 @@
+import duckdb
+import os
+
+
+def extract_data():
+    os.makedirs("data/bronze", exist_ok=True)
+
+    raw_data = duckdb.read_csv("data/bronze/electronics_sales_raw.csv")
+    raw_data.write_parquet("data/bronze/electronics_sales_raw.parquet")
+
+    print("Camada Bronze (Parquet) gerada com sucesso!")
+
+
+if __name__ == "__main__":
+    extract_data()
