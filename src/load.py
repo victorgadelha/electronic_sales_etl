e3310dc8ebf3d43570f09bff86dda48bfc833c69
Victor Gadelha <joaovictor_gadelha@live.com>
2026-04-27 11:41:48 -0300

    feat: criando análise de performance por produto

--- src/load.py
+++ src/load.py
@@ -17,6 +17,21 @@ def load_data():
         sum(net_income) AS lucro_liquido_total"""
     ).order("order_year_month")
 
+    product_performance = silver_data.aggregate(
+        """
+    category, 
+    brand, 
+    sum(net_revenue) AS faturamento_total, 
+    sum(gross_profit) AS lucro_total,
+    sum(quantity) AS qtd_total
+    """
+    ).order("faturamento_total DESC")
+
+    monthly_evolution.write_parquet("data/gold/monthly_evolution.parquet")
+    product_performance.write_parquet("data/gold/product_performance.parquet")
+
+    product_performance.limit(10).show()
+
 
 if __name__ == "__main__":
     load_data()
